ee797491b37e8b6f4ac0e102506a973ab89bfb65
SJB7788 <83483363+SJB7788@users.noreply.github.com>
2024-11-09 19:19:28 -0800

    set up side navbar and journal page

---
+++ Pages/Journal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AethersJournal.Pages
+{
+    public class JournalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
