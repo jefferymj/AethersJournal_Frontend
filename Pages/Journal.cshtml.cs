0fd9650984e71e35f9eccedd45ad3db10dd81959
SJB7788 <83483363+SJB7788@users.noreply.github.com>
2024-11-10 01:18:35 -0800

    aa

--- Pages/Journal.cshtml.cs
+++ Pages/Journal.cshtml.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,22 @@ namespace AethersJournal.Pages
 {
     public class JournalModel : PageModel
     {
+        public List<DateTime>? Dates {get; private set;}
+        
+        [FromRoute]
+        public string? Date {get; set;}
         public void OnGet()
         {
+            Dates = new List<DateTime>();
+            DateTime startDate = DateTime.Today.AddMonths(-1); 
+            DateTime endDate = DateTime.Today; 
+
+            for (DateTime date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                Dates.Add(date);
+            }
+
+            Dates.Reverse();
         }
     }
 }
